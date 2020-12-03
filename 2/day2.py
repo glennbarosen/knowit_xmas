9ca793f20ecc3d253d37d9390f6695b91e58f26f
GlennBarosen <glennbarosen@gmail.com>
2020-12-03 19:06:01 +0100

    not done

--- 2/day2.py
+++ 2/day2.py
@@ -1,19 +1,15 @@
 def isPrime(n):
-
-    # Corner cases
-    if (n <= 1):
+    if n <= 1:
         return False
-    if (n <= 3):
+    if n <= 3:
         return True
 
-    # This is checked so that we can skip
-    # middle five numbers in below loop
-    if (n % 2 == 0 or n % 3 == 0):
+    if n % 2 == 0 or n % 3 == 0:
         return False
 
     i = 5
-    while (i * i <= n):
-        if (n % i == 0 or n % (i + 2) == 0):
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
         i = i + 6
 
@@ -24,12 +20,11 @@ N = 5433000 - 1
 gifts = 0
 primes = []
 
-for i in range(0, 100):
-
+for i in range(0, 19):
     if (isPrime(i) == True):
         primes.append(i)
     if "7" in str(i):
-        if isPrime(i) == True:
+        if isPrime(i):
             gifts -= i + 1
         else:
             gifts -= primes[len(primes) - 1]
